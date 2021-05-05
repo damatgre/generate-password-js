ca87d3660f906dbb5cc83228501ac58d2d5a33b4
Greg D'Amato <gregdamato2@gmail.com>
2021-05-05 13:07:58 -0400

    changed string to array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Assignment code here
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-var num = "0123456789";
-var specialChar = "!()#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//arrays to pull individual characters
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lengthPass;
 var checkNum;
 var checkUpper;
